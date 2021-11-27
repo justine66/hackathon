b5ad04b7a162119dc5be21af404375110c24ee34
Cedric PATAT <75624402+CedricPatat@users.noreply.github.com>
2021-11-27 10:56:41 +0100

    Update stat_analysis.R

--- stat_analysis.R
+++ stat_analysis.R
@@ -1,50 +1,66 @@
-## Analysis Script
+###############################################################################
+########################      STATISTICAL ANALYSIS      #######################
+###############################################################################
 
-library(DESeq2)
+#### Description
+# This script performs a statistical analysis of the count matrix obtained by the workflow. It uses the DESeq2 and FactoMineR packages.
+# It takes the count file as input and returns various files as output. The files which are returned are a PCA graph, the table of the 
+# results of the differential expression analysis, a plot of the counts of the gene most differentially expressed between the two groups,
+# the heatmap of the normalized counts of the most variable genes and the name of these. genes in a text file.
 
-# Annotations
 
-#count_file = read.table("counts.txt", header = TRUE)
-count_file = read.table("output.counts", header=T)
+#### Library Loading
+install.packages("FactoMineR")  # Installation of the package to perform the PCA
+library(DESeq2)                 # Loading package for differential analysis
+library(FactoMineR)             # Loading packahe for the PCA
+
+
+#### Data Loading
+# Annotation Phenotype
+list_names=c("SRR628588", "SRR628587", "SRR628582", "SRR628584", "SRR628585", "SRR628583", "SRR628586", "SRR628589")      # List of individuals
+list_phenotype =c("Wild", "Wild", "Mutated", "Wild", "Wild", "Mutated", "Wild", "Mutated")                                # Phenotype for each individual
+phenotype = as.data.frame(matrix(list_phenotype, ncol = 1))
+rownames(phenotype) = list_names
+colnames(phenotype) = "Type"
+# Counts Matrix
+count_file = read.table(count_file, header=T)    # Loading count matrix file 
 counts = count_file[,7:14]
 rownames(counts) = count_file[,1]
-
-list_names = c("SRR628584.bam", "SRR628587.bam", "SRR628586.bam", "SRR628583.bam", "SRR628585.bam", "SRR628588.bam", "SRR628582.bam", "SRR628589.bam")
-list_phenotype = c("mutated", "wild", "wild", "mutated", "wild", "wild","mutated", "wild")
-
-phenotype = data.frame(list_phenotype[1:8])
-colnames(phenotype) = "type"
-rownames(phenotype) = list_names[1:8]
+colnames(counts) = list_names
 
 
+#### Analysis
 
-## supression des gènes "non exprimés"
-datexp= counts[rowSums(counts)>0,]
+## PCA
 
-## formatage de la matrice de comptage
-dataexp=as.matrix(datexp)
+res_pca = PCA(t(counts), graph= FALSE)              # Calculation of PCA
 
-## visualisation des log comptages
-hist(log(dataexp+1),col="blue")
-
-
-
-# construction de l'objet 
-dds <- DESeqDataSetFromMatrix(dataexp, DataFrame(phenotype), ~ type)
-dds <- estimateSizeFactors(dds)
-dds <- dds[ rowSums(counts(dds)) > 0, ]
-rld <- vst(dds, blind=TRUE)
-d.vst <- assay(rld)
+pdf("PCA_GraphOfIndividuals.pdf")
+plot(res_pca, axes = c(1, 2), choix = c("ind"))     # Calculation is made in 5 dimensions, and the display in 2 
+device.off()
 
 
-# Récupération des 1000 gènes qui varient le plus #
-gvar <- apply(d.vst, 1, var)
-mostvargenes <- order(gvar, decreasing=TRUE)[1:3000]
-d.vst_1000genes <- d.vst[mostvargenes,]
+## Differencial Analysis
 
+counts = counts[rowSums(counts)>0,]   # Removal of all unexpressed genes
+counts=as.matrix(counts)              # Converting the object's class
 
-pdf("heatmap.pdf")
-heatmap(d.vst_1000genes)
+dds = DESeqDataSetFromMatrix(countData = counts, colData = phenotype, design = ~ Type)  # Object construction
+dds = DESeq(dds)     # performs differential expression analysis
+res = results(dds)   # differential expression analysis
+res_mat = as.matrix(res)
+write.table(res_mat, "DESeq_results.txt", sep="\t")  # output file (table format txt) resulting of the differential expression analysis
+
+pdf("plot_counts.pdf")
+plotCounts(dds, gene=which.min(res$padj), intgroup="Type")  # output file (pdf) showing the most variable gene bewteen the 2 groups (wild and mutated)
 dev.off()
 
-write.table(d.vst_1000genes, "d.vst_1000genes.txt", sep="\t")
+select = order(res$padj, decreasing = FALSE)[1:50]  # Sorting and selection of the most variable genes
+
+pdf("heatmap_MostVariableGenes.pdf")
+heatmap(counts(dds, normalized=T)[select,], cexRow = 0.5, cexCol = 0.8)   # output file (pdf) showing normalized counts of the most variable genes for each individual
+dev.off()
+
+MostVariableGenes = rownames(counts[select,])  # Retrieving names names of the most variable genes
+
+write.table(MostVariableGenes, "MostVariableGenes.txt", sep=";")   # output file (list format txt) containing names of the most variable genes
