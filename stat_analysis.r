f91d2f10d195fa937d55a60e2785c974c2c3b824
Cedric PATAT <75624402+CedricPatat@users.noreply.github.com>
2021-11-28 14:00:38 +0100

    Update and rename stat_analysis.R to stat_analysis.r

--- stat_analysis.r
+++ stat_analysis.r
@@ -2,6 +2,7 @@
 ########################      STATISTICAL ANALYSIS      #######################
 ###############################################################################
 
+
 #### Description
 # This script performs a statistical analysis of the count matrix obtained by the workflow. It uses the DESeq2 and FactoMineR packages.
 # It takes the count file as input and returns various files as output. The files which are returned are a PCA graph, the table of the 
@@ -16,17 +17,32 @@ library(FactoMineR)             # Loading packahe for the PCA
 
 
 #### Data Loading
-# Annotation Phenotype
-list_names=c("SRR628588", "SRR628587", "SRR628582", "SRR628584", "SRR628585", "SRR628583", "SRR628586", "SRR628589")      # List of individuals
-list_phenotype =c("Wild", "Wild", "Mutated", "Wild", "Wild", "Mutated", "Wild", "Mutated")                                # Phenotype for each individual
-phenotype = as.data.frame(matrix(list_phenotype, ncol = 1))
-rownames(phenotype) = list_names
-colnames(phenotype) = "Type"
 # Counts Matrix
-count_file = read.table(count_file, header=T)    # Loading count matrix file 
+count_file = read.table("output.counts", header=T)    # Loading count matrix file 
 counts = count_file[,7:14]
 rownames(counts) = count_file[,1]
-colnames(counts) = list_names
+
+parserexpcolnames <- function(nomamodifier){     # remove ".bam" in colnames function
+  nomamodifier <- substring(nomamodifier, 1, 9)
+  return(nomamodifier)
+}
+
+colnames(counts) = sapply(colnames(counts), parserexpcolnames)   # remove ".bam" in colnames
+
+# Annotation Phenotype
+list_phenotype = c()   # list containing phenotype of each SRR
+for (i in 1:dim(counts)[2]){
+  if((colnames(counts)[i] == 'SRR628582') | (colnames(counts)[i] == 'SRR628583') | (colnames(counts)[i] == 'SRR628589')){
+    list_phenotype = c(list_phenotype, "Mutated")
+  }
+  else{
+    list_phenotype = c(list_phenotype, "Wild")
+  }
+}
+phenotype = as.data.frame(matrix(list_phenotype, ncol = 1))
+rownames(phenotype) = colnames(counts)
+colnames(phenotype) = "Type"
+
 
 
 #### Analysis
@@ -34,16 +50,16 @@ colnames(counts) = list_names
 ## PCA
 
 res_pca = PCA(t(counts), graph= FALSE)              # Calculation of PCA
-
 pdf("PCA_GraphOfIndividuals.pdf")
 plot(res_pca, axes = c(1, 2), choix = c("ind"))     # Calculation is made in 5 dimensions, and the display in 2 
-device.off()
+dev.off()
 
 
 ## Differencial Analysis
 
 counts = counts[rowSums(counts)>0,]   # Removal of all unexpressed genes
 counts=as.matrix(counts)              # Converting the object's class
+
 
 dds = DESeqDataSetFromMatrix(countData = counts, colData = phenotype, design = ~ Type)  # Object construction
 dds = DESeq(dds)     # performs differential expression analysis
@@ -55,10 +71,11 @@ pdf("plot_counts.pdf")
 plotCounts(dds, gene=which.min(res$padj), intgroup="Type")  # output file (pdf) showing the most variable gene bewteen the 2 groups (wild and mutated)
 dev.off()
 
-select = order(res$padj, decreasing = FALSE)[1:50]  # Sorting and selection of the most variable genes
+
+select = order(res$padj, decreasing = FALSE)[1:30]  # Sorting and selection of the most variable genes
 
 pdf("heatmap_MostVariableGenes.pdf")
-heatmap(counts(dds, normalized=T)[select,], cexRow = 0.5, cexCol = 0.8)   # output file (pdf) showing normalized counts of the most variable genes for each individual
+heatmap(counts(dds, normalized=T)[select,], cexRow = 0.5, cexCol = 0.8, Rowv = NA)   # output file (pdf) showing normalized counts of the most variable genes for each individual
 dev.off()
 
 MostVariableGenes = rownames(counts[select,])  # Retrieving names names of the most variable genes
